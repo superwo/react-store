3251b9888e1c7ca3e5ea4062de02b69ba5ef8d06
Radu <avadanii@tiscali.it>
2019-10-11 10:40:07 +0300

    initial setup

---
+++ pages/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import axios from 'axios';
+
+function Home({ products }) {
+  console.log(products);
+
+  return <>home</>;
+}
+
+Home.getInitialProps = async () => {
+  // fetch data on server
+  const url = 'http://localhost:3000/api/products';
+  const response = await axios.get(url);
+  //return response data as an object
+  return { products: response.data };
+  // note: this object will be merged with existing props
+};
+
+export default Home;
